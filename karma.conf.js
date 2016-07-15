6504ae9c57be2c2a6bf522133c7dcbe737c1bb00
Andrea Condoluci <andreacondoluci@gmail.com>
2016-07-15 15:33:43 +0200

    Fix Logging pt4

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,14 @@ module.exports = function (config) {
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_WARN,
+        logLevel: config.LOG_INFO,
+
+        client: {
+            captureConsole: true,
+            mocha: {
+                bail: true
+            }
+        },
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: false,
