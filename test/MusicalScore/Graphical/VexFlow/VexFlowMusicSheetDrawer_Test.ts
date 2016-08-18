2107971278cf356349ba69fa4f9c53ff92c277a5
Sebastian Haas <sebastian@haas.tech>
2016-08-18 14:23:35 +0200

    Uncommented unit tests again. Skipping four not working test cases.

--- test/MusicalScore/Graphical/VexFlow/VexFlowMusicSheetDrawer_Test.ts
+++ test/MusicalScore/Graphical/VexFlow/VexFlowMusicSheetDrawer_Test.ts
@@ -1,10 +1,12 @@
 import {VexFlowMusicSheetDrawer} from "../../../../src/MusicalScore/Graphical/VexFlow/VexFlowMusicSheetDrawer";
 import {GraphicalMusicSheet} from "../../../../src/MusicalScore/Graphical/GraphicalMusicSheet";
+import {OutlineAndFillStyleEnum} from "../../../../src/MusicalScore/Graphical/DrawingEnums";
 import {MusicSheet} from "../../../../src/MusicalScore/MusicSheet";
 import {MusicSheetReader} from "../../../../src/MusicalScore/ScoreIO/MusicSheetReader";
 import {VexFlowMusicSheetCalculator} from "../../../../src/MusicalScore/Graphical/VexFlow/VexFlowMusicSheetCalculator";
 import {TestUtils} from "../../../Util/TestUtils";
 import {IXmlElement} from "../../../../src/Common/FileIO/Xml";
+import {Fraction} from "../../../../src/Common/DataObjects/Fraction";
 
 describe("VexFlow Music Sheet Drawer", () => {
 
@@ -25,7 +27,7 @@ describe("VexFlow Music Sheet Drawer", () => {
         done();
     });
 
-    /*it("With cursor (as rectangle)", (done: MochaDone) => {
+    it.skip("With cursor (as rectangle)", (done: MochaDone) => {
         let score: Document = TestUtils.getScore("MuzioClementi_SonatinaOpus36No1_Part1");
         chai.expect(score).to.not.be.undefined;
         let partwise: Element = TestUtils.getPartWiseElement(score);
@@ -41,5 +43,5 @@ describe("VexFlow Music Sheet Drawer", () => {
         let drawer: VexFlowMusicSheetDrawer = new VexFlowMusicSheetDrawer(canvas);
         drawer.drawSheet(gms);
         done();
-    });*/
+    });
 });
