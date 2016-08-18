a2a4763a7856cefc5df22da19697d58837a28a08
Sebastian Haas <sebastian@haas.tech>
2016-08-18 15:19:42 +0200

    Updated publish_docs.sh script. #10

--- bin/publish_docs.sh
+++ bin/publish_docs.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+grunt docs
 git clone git@github.com:opensheetmusicdisplay/opensheetmusicdisplay.github.io.git
-rsync -a ./build/docs ./opensheetmusicdisplay.github.io.git
+cd opensheetmusicdisplay.github.io
+git status
+rsync -a ../build/docs/* ./
 git status
 git add *
 git commit -m "Pushed auto-generated class documentation for $TRAVIS_TAG"
 git tag -a $TRAVIS_TAG -m "Class documentation for $TRAVIS_TAG"
 git push origin master --follow-tags
+echo "Deployed class documentation for $TRAVIS_TAG successfully."
+cd ..
+rm -rf opensheetmusicdisplay.github.io
