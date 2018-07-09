fd2f2bba34de153e78d5b9e1915b376553907e2a
sschmidTU <simon.schmid.bsc@gmail.com>
2018-07-09 15:28:14 +0200

    removed isGrace from GraphicalVoiceEntry

--- src/MusicalScore/Graphical/GraphicalVoiceEntry.ts
+++ src/MusicalScore/Graphical/GraphicalVoiceEntry.ts
@@ -8,17 +8,15 @@ import { GraphicalStaffEntry } from "./GraphicalStaffEntry";
  * The graphical counterpart of a [[VoiceEntry]].
  */
 export class GraphicalVoiceEntry extends GraphicalObject {
-    constructor(parentVoiceEntry: VoiceEntry, parentStaffEntry: GraphicalStaffEntry, isGrace: boolean = false) {
+    constructor(parentVoiceEntry: VoiceEntry, parentStaffEntry: GraphicalStaffEntry) {
         super();
         this.parentVoiceEntry = parentVoiceEntry;
         this.parentStaffEntry = parentStaffEntry;
         this.PositionAndShape = new BoundingBox(this, parentStaffEntry ? parentStaffEntry.PositionAndShape : undefined);
         this.notes = [];
-        this.isGrace = isGrace;
     }
 
     public parentVoiceEntry: VoiceEntry;
     public parentStaffEntry: GraphicalStaffEntry;
     public notes: GraphicalNote[];
-    public isGrace: boolean;
 }
