962d24ea6d5b56d2ef07d48d04a48687bf337134
sschmidTU <simon.schmid.bsc@gmail.com>
2018-08-30 16:03:33 +0200

    change back label placement for non-lyrics to previous state (fix previous commit)

--- src/MusicalScore/Graphical/GraphicalLabel.ts
+++ src/MusicalScore/Graphical/GraphicalLabel.ts
@@ -76,15 +76,15 @@ export class GraphicalLabel extends Clickable {
                 break;
             case TextAlignmentAndPlacement.LeftCenter:
                 bbox.BorderTop = -height / 2;
-                bbox.BorderLeft = -1;
+                bbox.BorderLeft = 0;
                 bbox.BorderBottom = height / 2;
-                bbox.BorderRight = width - 1;
+                bbox.BorderRight = width;
                 break;
             case TextAlignmentAndPlacement.LeftTop:
                 bbox.BorderTop = 0;
-                bbox.BorderLeft = -1;
+                bbox.BorderLeft = 0;
                 bbox.BorderBottom = height;
-                bbox.BorderRight = width - 1;
+                bbox.BorderRight = width;
                 break;
             case TextAlignmentAndPlacement.RightBottom:
                 bbox.BorderTop = -height;
