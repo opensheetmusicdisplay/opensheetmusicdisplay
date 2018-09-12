b29cf32286b6e38902a3d7dc56149e8052731910
sschmidTU <simon.schmid.bsc@gmail.com>
2018-09-12 15:06:43 +0200

    refactor: combine RegExes into one

--- src/Common/Strings/StringUtil.ts
+++ src/Common/Strings/StringUtil.ts
@@ -1,9 +1,9 @@
 ﻿export class StringUtil {
   public static StringContainsSeparatedWord(str: string, wordRegExString: string): boolean {
-    if (new RegExp("^" + wordRegExString + "$").test(str) ||
-      new RegExp(" " + wordRegExString).test(str) ||
-      new RegExp(wordRegExString + " ").test(str) ||
-      new RegExp(wordRegExString + "\\.").test(str)) {
+    if (new RegExp("(^" + wordRegExString + "$)|" + // exact match
+      "( " + wordRegExString + ")|" + // " " + str
+      "(" + wordRegExString + "[ .])" // str + " " or "."
+      ).test(str)) {
       return true;
     }
     return false;
