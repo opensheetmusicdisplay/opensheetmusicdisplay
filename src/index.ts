8cab9599e58bcc2113a7f5b4c63db78731662898
sschmidTU <simon.schmid.bsc@gmail.com>
2018-11-12 15:29:09 +0100

    test-wise, export a few classes for import in typescript projects
    tested in webscript-usage-example project, can import and use Pitch for example

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { OpenSheetMusicDisplay } from "./OpenSheetMusicDisplay/OpenSheetMusicDisplay";
+export * from "./Common/";
+export * from "./MusicalScore/Graphical/EngravingRules";
+export * from "./MusicalScore/Graphical/GraphicalNote";
+export * from "./MusicalScore/VoiceData/Note";
+export * from "./MusicalScore/Label";
+export * from "./OpenSheetMusicDisplay/Cursor";
+//export * from "./Util/";
