53f2c44f829371eaa2724f5a876fca714d0a5d0f
Benjamin Giesinger <benjamin.giesinger@gmail.com>
2019-02-26 20:21:29 +0100

    feat(Octavas use vexflow types now): Using types from Definitely typed

--- src/MusicalScore/Graphical/VexFlow/CanvasVexFlowBackend.ts
+++ src/MusicalScore/Graphical/VexFlow/CanvasVexFlowBackend.ts
@@ -118,6 +118,6 @@ export class CanvasVexFlowBackend extends VexFlowBackend {
         // This clusterfuck is only there to counter act my favorite vexflow line:
         // ctx.vexFlowCanvasContext = ctx;
         // No idea why they are saving the context but we wrap the types here
-        return <CanvasRenderingContext2D>(ctx as any).vexFlowCanvasContext;
+        return <CanvasRenderingContext2D>(this.ctx as any).vexFlowCanvasContext;
     }
 }
