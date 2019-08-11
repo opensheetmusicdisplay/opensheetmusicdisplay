5c4f5dc6a0f9abc1cea85aa3186f34a6494c4047
praisethemoon <doit@praisethemoon.org>
2019-08-11 10:30:24 +0100

    Fixed tiny confusion between `userStartTempoInBPM` and `userStartTempoInBPM`

--- src/MusicalScore/ScoreIO/MusicSheetReader.ts
+++ src/MusicalScore/ScoreIO/MusicSheetReader.ts
@@ -183,7 +183,8 @@ export class MusicSheetReader /*implements IMusicSheetReader*/ {
          afterSheetReadingModule.calculate(this.musicSheet);
         }
 
-        this.musicSheet.userStartTempoInBPM = this.musicSheet.SourceMeasures[0].TempoInBPM;
+        this.musicSheet.DefaultStartTempoInBpm = this.musicSheet.SourceMeasures[0].TempoInBPM;
+        this.musicSheet.userStartTempoInBPM = this.musicSheet.userStartTempoInBPM || this.musicSheet.DefaultStartTempoInBpm;
 
         return this.musicSheet;
     }
