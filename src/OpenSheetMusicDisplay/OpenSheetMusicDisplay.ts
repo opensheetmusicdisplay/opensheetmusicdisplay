772be0ddaf4759bac3ace744308afc393c8a71d5
sschmid <s.schmid@phonicscore.com>
2019-08-26 17:39:38 +0200

    chore: bump version to 0.7.1b (demo fix)

--- src/OpenSheetMusicDisplay/OpenSheetMusicDisplay.ts
+++ src/OpenSheetMusicDisplay/OpenSheetMusicDisplay.ts
@@ -27,7 +27,7 @@ import {AutoColorSet} from "../MusicalScore";
  * After the constructor, use load() and render() to load and render a MusicXML file.
  */
 export class OpenSheetMusicDisplay {
-    private version: string = "0.7.1-dev"; // getter: this.Version
+    private version: string = "0.7.1b-release"; // getter: this.Version
     // at release, bump version and change to -release, afterwards to -dev again
 
     /**
