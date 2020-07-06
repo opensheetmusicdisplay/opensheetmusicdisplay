1b39c88b0d650432d87e0f15bbd11ecc45896042
Gregg Larson <larsongb@gmail.com>
2020-07-05 18:04:02 -0600

    changes for adding a tie

--- src/MusicalScore/Graphical/MusicSheetCalculator.ts
+++ src/MusicalScore/Graphical/MusicSheetCalculator.ts
@@ -420,7 +420,7 @@ export abstract class MusicSheetCalculator {
      * @param tie
      * @param tieIsAtSystemBreak
      */
-    protected layoutGraphicalTie(tie: GraphicalTie, tieIsAtSystemBreak: boolean): void {
+    protected layoutGraphicalTie(tie: GraphicalTie, tieIsAtSystemBreak: boolean, isTab: boolean): void {
         throw new Error("abstract, not implemented");
     }
 
@@ -2382,7 +2382,7 @@ export abstract class MusicSheetCalculator {
                                     graphicalTie.StartNote.parentVoiceEntry.parentStaffEntry.parentMeasure.ParentStaffLine !==
                                     graphicalTie.EndNote.parentVoiceEntry.parentStaffEntry.parentMeasure.ParentStaffLine
                                 );
-                                this.layoutGraphicalTie(graphicalTie, tieIsAtSystemBreak);
+                                this.layoutGraphicalTie(graphicalTie, tieIsAtSystemBreak, measure.ParentStaff.isTab);
                             }
                         }
                     }
