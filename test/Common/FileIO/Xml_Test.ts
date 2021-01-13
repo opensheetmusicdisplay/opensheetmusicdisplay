0917540cca069c15759a12bdfd01f58beb39c6f8
Michael Hartman <michael.p.hartman@gmail.com>
2021-01-13 02:16:22 -0500

    added test files

--- test/Common/FileIO/Xml_Test.ts
+++ test/Common/FileIO/Xml_Test.ts
@@ -37,6 +37,10 @@ describe("XML interface", () => {
         "ScottJoplin_The_Entertainer.xml",
         "TelemannWV40.102_Sonate-Nr.1.1-Dolce.xml",
         "TelemannWV40.102_Sonate-Nr.1.2-Allegro-F-Dur.xml",
+        "ChordSpacingTest.musicxml",
+        "sus4_test.xml",
+        "Cmmaj9_test.xml",
+        "Butterfly.musicxml",
     ];
     for (const score of xmlTestset) {
         testFile(score);
