cf7054b01159fd90f2e6ed43c50228c143ac9756
Michael Hartman <michael.p.hartman@gmail.com>
2021-01-22 17:16:38 -0500

    Compress ChordSpacingTest and BrookeWest samples

--- test/Common/FileIO/Xml_Test.ts
+++ test/Common/FileIO/Xml_Test.ts
@@ -37,7 +37,6 @@ describe("XML interface", () => {
         "ScottJoplin_The_Entertainer.xml",
         "TelemannWV40.102_Sonate-Nr.1.1-Dolce.xml",
         "TelemannWV40.102_Sonate-Nr.1.2-Allegro-F-Dur.xml",
-        "ChordSpacingTest.musicxml",
         "VariousChordTests.musicxml",
     ];
     for (const score of xmlTestset) {
