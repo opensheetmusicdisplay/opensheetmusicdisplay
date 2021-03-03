fd07d76069195845730759a93b4b099454b91f75
ryohorie <develop@ryohorie.com>
2021-03-03 20:25:34 +0900

    fix chord measure with too large

--- src/MusicalScore/Graphical/MusicSheetCalculator.ts
+++ src/MusicalScore/Graphical/MusicSheetCalculator.ts
@@ -307,7 +307,7 @@ export abstract class MusicSheetCalculator {
             for (let i: number = 1; i < this.graphicalMusicSheet.MeasureList.length; i++) {
                 measures = this.graphicalMusicSheet.MeasureList[i];
                 minimumStaffEntriesWidth = this.calculateMeasureXLayout(measures);
-                minimumStaffEntriesWidth = this.calculateMeasureWidthFromStaffEntries(measures, minimumStaffEntriesWidth);
+                // minimumStaffEntriesWidth = this.calculateMeasureWidthFromStaffEntries(measures, minimumStaffEntriesWidth);
                 MusicSheetCalculator.setMeasuresMinStaffEntriesWidth(measures, minimumStaffEntriesWidth);
                 // minLength = Math.max(minLength, minimumStaffEntriesWidth * 1.2 + maxInstructionsLength);
             }
