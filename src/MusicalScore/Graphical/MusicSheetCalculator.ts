d3588b4b5320a7333a41d693f26e7ac8f1d88e3a
fredmeister77 <fredmeister77@gmail.com>
2021-03-04 11:15:37 -0500

    Fixes for transposition
    don't display accidentals when transposing back

--- src/MusicalScore/Graphical/MusicSheetCalculator.ts
+++ src/MusicalScore/Graphical/MusicSheetCalculator.ts
@@ -2168,8 +2168,7 @@ export abstract class MusicSheetCalculator {
                 const instruction: AbstractNotationInstruction = sourceMeasure.FirstInstructionsStaffEntries[staffIndex].Instructions[idx];
                 if (instruction instanceof KeyInstruction) {
                     const key: KeyInstruction = KeyInstruction.copy(instruction);
-                    if (this.graphicalMusicSheet.ParentMusicSheet.Transpose !== 0 &&
-                        measure.ParentStaff.ParentInstrument.MidiInstrumentId !== MidiInstrument.Percussion &&
+                    if (measure.ParentStaff.ParentInstrument.MidiInstrumentId !== MidiInstrument.Percussion &&
                         MusicSheetCalculator.transposeCalculator) {
                         MusicSheetCalculator.transposeCalculator.transposeKey(
                             key, this.graphicalMusicSheet.ParentMusicSheet.Transpose
