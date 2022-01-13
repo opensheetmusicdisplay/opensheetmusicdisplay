e0137374428e327795d6a9689390ec67a0c19574
sschmidTU <s.schmid@phonicscore.com>
2022-01-13 17:55:59 +0100

    test: add test for stem/beam SVG ids (#1108)

--- test/MusicalScore/Graphical/VexFlow/VexFlowMeasure_Test.ts
+++ test/MusicalScore/Graphical/VexFlow/VexFlowMeasure_Test.ts
@@ -10,6 +10,8 @@ import {SourceStaffEntry} from "../../../../src/MusicalScore/VoiceData/SourceSta
 import {GraphicalMeasure} from "../../../../src/MusicalScore/Graphical/GraphicalMeasure";
 import {MusicSheetCalculator} from "../../../../src/MusicalScore/Graphical/MusicSheetCalculator";
 import {EngravingRules} from "../../../../src/MusicalScore/Graphical/EngravingRules";
+import { GraphicalStaffEntry } from "../../../../src/MusicalScore/Graphical/GraphicalStaffEntry";
+import { VexFlowGraphicalNote } from "../../../../src";
 
 describe("VexFlow Measure", () => {
 
@@ -54,6 +56,27 @@ describe("VexFlow Measure", () => {
       chai.expect(gms.MeasureList.length).to.equal(1);
       chai.expect(gms.MeasureList[0].length).to.equal(1);
       chai.expect(gms.MeasureList[0][0].staffEntries.length).to.equal(0);
+      done();
+   });
+
+   it("Stem and Beam SVG id", (done: Mocha.Done) => {
+      const path: string = "test_rest_positioning_8th_quarter.musicxml";
+      // the test sample should simply be one that starts with a beamed 8th note (and is small).
+      const score: Document = TestUtils.getScore(path);
+      const partwise: Element = TestUtils.getPartWiseElement(score);
+      const reader: MusicSheetReader = new MusicSheetReader();
+      const calc: VexFlowMusicSheetCalculator = new VexFlowMusicSheetCalculator(reader.rules);
+      const sheet: MusicSheet = reader.createMusicSheet(new IXmlElement(partwise), path);
+      const gms: GraphicalMusicSheet = new GraphicalMusicSheet(sheet, calc);
+      const gse: GraphicalStaffEntry = gms.findGraphicalMeasure(0, 0).staffEntries[0];
+      const firstNote: VexFlowGraphicalNote = (gse.graphicalVoiceEntries[0].notes[0] as VexFlowGraphicalNote);
+      const noteSVGId: string = firstNote.getSVGId();
+      const stemSVGId: string = noteSVGId + "-stem";
+      const stemSVG: HTMLElement = document.getElementById(stemSVGId);
+      const beamSVGId: string = noteSVGId + "-beam";
+      const beamSVG: HTMLElement = document.getElementById(beamSVGId);
+      chai.expect(stemSVG).to.not.be.undefined;
+      chai.expect(beamSVG).to.not.be.undefined;
       done();
    });
 
