915c1a38eddba8c7a0bb52587b5ec0ec5f48d75f
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-03-22 14:13:46 +0900

    Make FragmentShader.glsl support multiple rows

--- src/MusicalScore/Graphical/Shaders/FragmentShader.glsl
+++ src/MusicalScore/Graphical/Shaders/FragmentShader.glsl
@@ -2,28 +2,38 @@ precision mediump float;
 uniform sampler2D u_image;
 varying vec4 v_position;
 
+#define NUM_ROWS 5
+
 void main() {
-    vec2 realPosition = (v_position.xy + vec2(1.0)) / vec2(2.0);
-    float x = realPosition.x;
+    vec2 absolutePosition = (v_position.xy + vec2(1.0)) / vec2(2.0);
+    float absX = absolutePosition.x;
+    float absY = absolutePosition.y;
+
     float skyLine = 0.0;
     for (int i = 0; i < 300; ++i) {
-        float y = float(i) / 299.0;
+        float relY = (float(i) / 299.0 - 0.5) / NUM_ROWS;
+        float x = absX;
+        float y = absY + relY;
+
         float currentAlpha = texture2D(u_image, vec2(x, y)).a;
         if (currentAlpha > 0.0) {
             skyLine = y;
             break;
         }
     }
-    
+
     float bottomLine = 1.0;
     for (int i = 299; i >= 0; --i) {
-        float y = float(i) / 299.0;
+        float relY = (float(i) / 299.0 - 0.5) / NUM_ROWS;
+        float x = absX;
+        float y = absY + relY;
+
         float currentAlpha = texture2D(u_image, vec2(x, y)).a;
         if (currentAlpha > 0.0) {
             bottomLine = y;
             break;
         }
     }
-    
+
     gl_FragColor = vec4(skyLine, bottomLine, 1, 1);
 }
