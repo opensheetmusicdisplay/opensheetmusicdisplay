1cb560cfa8e8e5be9417873c38112cf40c5909cc
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-03-23 05:50:36 +0900

    Fix wrong logic in FragmentShader.glsl

--- src/MusicalScore/Graphical/Shaders/FragmentShader.glsl
+++ src/MusicalScore/Graphical/Shaders/FragmentShader.glsl
@@ -11,26 +11,28 @@ void main() {
 
     float skyLine = 0.0;
     for (int i = 0; i < 300; ++i) {
-        float relY = (float(i) / 299.0 - 0.5) / NUM_ROWS;
+        float ratioY = float(i) / 299.0;
+        float relY = (ratioY - 0.5) / float(NUM_ROWS);
         float x = absX;
         float y = absY + relY;
 
         float currentAlpha = texture2D(u_image, vec2(x, y)).a;
         if (currentAlpha > 0.0) {
-            skyLine = y;
+            skyLine = ratioY;
             break;
         }
     }
 
     float bottomLine = 1.0;
     for (int i = 299; i >= 0; --i) {
-        float relY = (float(i) / 299.0 - 0.5) / NUM_ROWS;
+        float ratioY = float(i) / 299.0;
+        float relY = (ratioY - 0.5) / float(NUM_ROWS);
         float x = absX;
         float y = absY + relY;
 
         float currentAlpha = texture2D(u_image, vec2(x, y)).a;
         if (currentAlpha > 0.0) {
-            bottomLine = y;
+            bottomLine = ratioY;
             break;
         }
     }
