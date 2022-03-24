05878f8965be79ca04ceb9cb2b744d222397bf52
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-03-24 19:09:47 +0900

    Replace logic in FragmentShader.glsl with more precise one

--- src/MusicalScore/Graphical/Shaders/FragmentShader.glsl
+++ src/MusicalScore/Graphical/Shaders/FragmentShader.glsl
@@ -6,14 +6,16 @@ varying vec4 v_position;
 #define ELEMENT_HEIGHT 300
 
 void main() {
+    const float halfPixel = 1.0 / float(ELEMENT_HEIGHT * 2);
+
     vec2 absolutePosition = (v_position.xy + vec2(1.0)) / vec2(2.0);
     float absX = absolutePosition.x;
     float absY = absolutePosition.y;
 
     float skyLine = 0.0;
     for (int i = 0; i < ELEMENT_HEIGHT; ++i) {
-        float ratioY = float(i) / float(ELEMENT_HEIGHT - 1);
-        float relY = (ratioY - 0.5) / float(NUM_ROWS);
+        float ratioY = float(i) / float(ELEMENT_HEIGHT);
+        float relY = (ratioY - 0.5 + halfPixel) / float(NUM_ROWS);
         float x = absX;
         float y = absY + relY;
 
@@ -25,9 +27,9 @@ void main() {
     }
 
     float bottomLine = 1.0;
-    for (int i = ELEMENT_HEIGHT - 1; i >= 0; --i) {
-        float ratioY = float(i) / float(ELEMENT_HEIGHT - 1);
-        float relY = (ratioY - 0.5) / float(NUM_ROWS);
+    for (int i = ELEMENT_HEIGHT - 2; i >= 0; --i) {
+        float ratioY = float(i + 1) / float(ELEMENT_HEIGHT);
+        float relY = (ratioY - 0.5 + halfPixel) / float(NUM_ROWS);
         float x = absX;
         float y = absY + relY;
 
