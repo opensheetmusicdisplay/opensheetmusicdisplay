d32bc1a437e700be32b61b40cb45807161908620
sschmidTU <s.schmid@phonicscore.com>
2022-07-06 15:29:45 +0200

    fix(Rehearsal Mark): Position correctly when EngravingRules.RehearsalMarkFontSize is increased (#1176)
    not visual regression tested yet
    thanks to johanvelthuis for the suggestion.

--- src/VexFlowPatch/src/stavesection.js
+++ src/VexFlowPatch/src/stavesection.js
@@ -38,16 +38,16 @@ export class StaveSection extends StaveModifier {
     const text_height = text_measurements.height;
     let width = text_width + 6;  // add left & right padding
     if (width < 18) width = 18;
-    const height = text_height;
+    const height = text_height + this.font.size / 10; // font.size / 10: padding
     //  Seems to be a good default y
-    const y = stave.getYForTopText(3) + this.shift_y;
+    const y = stave.getYForTopText(3) + 16 - (height * 1.15) + this.shift_y;
     let x = this.x + shift_x;
     ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.rect(x, y + text_height/4, width, height);
     ctx.stroke();
     x += (width - text_width) / 2;
-    ctx.fillText('' + this.section, x, y + 16);
+    ctx.fillText('' + this.section, x, y + height );
     ctx.restore();
     return this;
   }
