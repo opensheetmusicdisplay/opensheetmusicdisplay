dac1fab55eb42e2c32e69444d78fe71a39963058
sschmidTU <s.schmid@phonicscore.com>
2023-01-24 22:13:00 +0100

    chore: generateImages: check for uppercase

--- test/Util/generateImages_browserless.mjs
+++ test/Util/generateImages_browserless.mjs
@@ -34,6 +34,7 @@ function sleep (ms) {
 //   (without these being global, we'd have to pass many of these values to the generateSampleImage function)
 // eslint-disable-next-line prefer-const
 let [osmdBuildDir, sampleDir, imageDir, imageFormat, pageWidth, pageHeight, filterRegex, mode, debugSleepTimeString, skyBottomLinePreference] = process.argv.slice(2, 12);
+imageFormat = imageFormat?.toLowerCase();
 if (!osmdBuildDir || !sampleDir || !imageDir || (imageFormat !== "png" && imageFormat !== "svg")) {
     console.log("usage: " +
         // eslint-disable-next-line max-len
