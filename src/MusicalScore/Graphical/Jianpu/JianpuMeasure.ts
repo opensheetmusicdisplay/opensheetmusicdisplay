bebecf0b3bf99b7b81823ecbbb16dc67654b07f2
sschmidTU <s.schmid@phonicscore.com>
2023-03-03 03:13:11 +0100

    refactor: remove (currently) duplicate code
    might become relevant again whether we're directly under the note label, in which case we'll need if/else again

--- src/MusicalScore/Graphical/Jianpu/JianpuMeasure.ts
+++ src/MusicalScore/Graphical/Jianpu/JianpuMeasure.ts
@@ -155,13 +155,9 @@ export class JianpuMeasure extends VexFlowMeasure {
                             const rectangle: GraphicalRectangle = new GraphicalRectangle(
                                 startPoint, endPoint, ve.PositionAndShape, OutlineAndFillStyleEnum.BaseWritingColor);
                             rectangle.PositionAndShape.RelativePosition.x = this.rules.JianpuUnderlineXOffset;
-                            if (lastBbox.DataObject === noteGLabel) {
-                                rectangle.PositionAndShape.RelativePosition.y = addedYPositionBelow; // 0, on purpose. 0 is under the note
-                                addedYPositionBelow += rectangle.PositionAndShape.BorderBottom + this.rules.JianpuUnderlineWidth * 2.5; // + margin
-                            } else {
-                                rectangle.PositionAndShape.RelativePosition.y = addedYPositionBelow;
-                                addedYPositionBelow += rectangle.PositionAndShape.BorderBottom + this.rules.JianpuUnderlineWidth * 2.5; // + margin;
-                            }
+                            // if (lastBbox.DataObject === noteGLabel) { // irrelevant for now
+                            rectangle.PositionAndShape.RelativePosition.y = addedYPositionBelow; // 0, on purpose. 0 is under the note
+                            addedYPositionBelow += rectangle.PositionAndShape.BorderBottom + this.rules.JianpuUnderlineWidth * 2.5; // + margin
                             //const line: GraphicalLine = new GraphicalLine(startPoint, endPoint, this.rules.JianpuUnderlineWidth);
                             note.JianpuRectangles.push(rectangle);
                             se.JianpuNoteRectangles.push(rectangle);
