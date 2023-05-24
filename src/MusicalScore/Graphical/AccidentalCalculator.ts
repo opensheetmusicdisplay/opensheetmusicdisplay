9cad94401b077f21f855fcb46fee10922d700cec
Christian Baumann <33223146+chbauman@users.noreply.github.com>
2023-05-24 19:14:11 +0200

    Added additional check
    The previous accidental must not be discarded if the current alteration is also a natural.

--- src/MusicalScore/Graphical/AccidentalCalculator.ts
+++ src/MusicalScore/Graphical/AccidentalCalculator.ts
@@ -91,7 +91,7 @@ export class AccidentalCalculator {
                 if (this.isAlterAmbiguousAccidental(pitch.Accidental) && ! pitch.AccidentalXml) {
                     return; // only display accidental if it was given as an accidental in the XML
                 }
-                if (pitch.Accidental === AccidentalEnum.NATURAL) {
+                if (pitch.Accidental === AccidentalEnum.NATURAL && inMeasureAlterationAccidental !== AccidentalEnum.NATURAL) {
                     // If a natural is added, clear alterations on the same pitch for the current measure.
                     this.currentInMeasureNoteAlterationsDict.remove(pitchKey);
                 }
