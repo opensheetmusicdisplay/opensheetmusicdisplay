3a4abf2920e80eacccd133a65114e1c392785670
ammatwain <amedeo@sorpreso.it>
2023-07-30 10:35:16 +0200

    update packaje.json & ETC.webpack.prod.js

--- ETC.webpack.prod.js
+++ ETC.webpack.prod.js
@@ -4,15 +4,17 @@ var path = require('path')
 var common = require('./ETC.webpack.common.js')
 var Visualizer = require('webpack-visualizer-plugin2')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// eslint-disable-next-line @typescript-eslint/typedef
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
     output: {
-        filename: '[name].min.js',
-        path: path.resolve(__dirname, 'ETC-build'),
-        library: 'opensheetmusicdisplay',
-        libraryTarget: 'umd'
+        filename: "[name].min.js",
+        path: path.resolve(__dirname, "ETC-build"),
+        library: "opensheetmusicdisplay",
+        libraryTarget: "umd"
     },
-    mode: 'production',
+    mode: "production",
     optimization: {
         minimize: true
         // splitChunks: {
@@ -25,15 +27,20 @@ module.exports = merge(common, {
         new CleanWebpackPlugin({
             verbose: false,
             dry: false,
-            cleanOnceBeforeBuildPatterns: ['**/*', '!statistics.html*']
+            cleanOnceBeforeBuildPatterns: ["**/*", "!statistics.html*"]
         }),
         new webpack.LoaderOptionsPlugin({
             minimize: true,
             debug: true
         }),
         new Visualizer({
-            path: path.resolve(__dirname, 'ETC-build'),
-            filename: './statistics.html'
-        })
+            path: path.resolve(__dirname, "ETC-build"),
+            filename: "./statistics.html",
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, "test/data"), to: path.resolve(__dirname, "ETC-build/sheets")},
+            ],
+        }),
     ]
 })
