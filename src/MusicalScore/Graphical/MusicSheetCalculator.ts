a60add250431909d9a074c771c09a6f66548827b
sschmidTU <s.schmid@phonicscore.com>
2024-01-09 18:18:06 +0100

    only add startCollideBox.BorderRight if on the same ParentStaffline (workaround)
    the more fundamental solution would be to fix dynamicExpressionMap mapping across stafflines.

--- src/MusicalScore/Graphical/MusicSheetCalculator.ts
+++ src/MusicalScore/Graphical/MusicSheetCalculator.ts
@@ -1406,6 +1406,11 @@ export abstract class MusicSheetCalculator {
             this.dynamicExpressionMap.get(graphicalContinuousDynamic.ContinuousDynamic.StartMultiExpression.AbsoluteTimestamp.RealValue);
         if (startCollideBox) {
             startPosInStaffline.x = startCollideBox.RelativePosition.x + this.rules.WedgeHorizontalMargin;
+            if ((startCollideBox.DataObject as any).ParentStaffLine === staffLine) {
+                // issue: the dynamicExpressionMap doesn't distinguish between staffLines, so we may react to a different staffline otherwise
+                //   the more fundamental solution would be to fix dynamicExpressionMap mapping across stafflines.
+                startPosInStaffline.x += startCollideBox.BorderRight;
+            }
         }
         //currentMusicSystem and currentStaffLine
         const musicSystem: MusicSystem = staffLine.ParentMusicSystem;
