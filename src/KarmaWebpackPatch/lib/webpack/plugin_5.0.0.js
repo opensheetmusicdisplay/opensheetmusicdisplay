7f27317be880b29c576fecf5c0ec36e7ed29cc87
sschmidTU <s.schmid@phonicscore.com>
2024-02-01 23:09:50 +0100

    chore: remove karma-webpack patch entirely, as 5.0.1 seems to work and have fixed the MacOS build issue

--- src/KarmaWebpackPatch/lib/webpack/plugin_5.0.0.js
+++
@@ -1,9 +0,0 @@
-const KW_Framework = require('../karma-webpack/framework');
-const KW_Preprocessor = require('../karma-webpack/preprocessor');
-
-const KW_KarmaPlugin = {
-  'preprocessor:webpack': ['factory', KW_Preprocessor],
-  'framework:webpack': ['factory', KW_Framework],
-};
-
-module.exports = KW_KarmaPlugin;
