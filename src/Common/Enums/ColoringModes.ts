fff7a31745387de260e85af8fe261b71fbd882a8
Simon <33069673+sschmidTU@users.noreply.github.com>
2024-02-16 00:31:26 +0100

    refactor: Move enums used in OSMDOptions to common (#1517) to simplify imports
    * refactor: move DrawingParametersEnum to common, so that importing OSMDOptions doesn't cause import chain to EngravingRules etc
    
    same will be done for ColoringModes
    
    * refactor: move ColoringModes enum to common, so that importing OSMDOptions doesn't cause big import chain
    
    * refactor: add ColoringModes (enum class) to module exports
    
    almost forgot! always check when there's a new class.
    
    * comment (to restart appveyor build)
    
    appveyor build failed due to connection issues or sth
    
    ---------
    
    Co-authored-by: sschmidTU <s.schmid@phonicscore.com>

---
+++ src/Common/Enums/ColoringModes.ts
@@ -0,0 +1,6 @@
+export enum ColoringModes {
+    XML = 0,
+    AutoColoring = 1,
+    CustomColorSet = 2
+}
+// note that this is moved here from DrawingParameters to prevent an import chain to EngravingRules etc
