3722cc63bc3f6493c7b1d47ce8a23751ac34dd90
sschmidTU <s.schmid@phonicscore.com>
2024-04-05 14:26:32 +0200

    feat(OnXMLRead): Make the function public, add definition and jsdoc. (allows to modify XML before parsing)

--- src/OpenSheetMusicDisplay/OpenSheetMusicDisplay.ts
+++ src/OpenSheetMusicDisplay/OpenSheetMusicDisplay.ts
@@ -91,7 +91,11 @@ export class OpenSheetMusicDisplay {
     protected autoResizeEnabled: boolean;
     protected resizeHandlerAttached: boolean;
     protected followCursor: boolean;
-    protected OnXMLRead: Function;
+    /** A function that is executed when the XML has been read.
+     * The return value will be used as the actual XML OSMD parses,
+     * so you can make modifications to the xml that OSMD will use.
+     * Note that this is (re-)set on osmd.setOptions as `{return xml}`, unless you specify the function in the options. */
+    public OnXMLRead: (xml: string) => string;
 
     /**
      * Load a MusicXML file
