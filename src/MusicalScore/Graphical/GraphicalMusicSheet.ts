opensheetmusicdisplay/opensheetmusicdisplay

commit c86587eb4f454b5bbde97a5daf3aa41f64bd3a05

fix(GetNearestStaffEntry): Fix sometimes a non-staffentry being returned (e.g. voice entry)

src/MusicalScore/Graphical/GraphicalMusicSheet.ts
@@ -688,7 +688,8 @@ export class GraphicalMusicSheet {
         // Search for StaffEntries in region
         for (let idx: number = 0, len: number = this.MusicPages.length; idx < len; ++idx) {
             const graphicalMusicPage: GraphicalMusicPage = this.MusicPages[idx];
-            const entries: GraphicalStaffEntry[] = graphicalMusicPage.PositionAndShape.getObjectsInRegion<GraphicalStaffEntry>(region, false);
+            const entries: GraphicalStaffEntry[] = graphicalMusicPage.PositionAndShape.
+                getObjectsInRegion<GraphicalStaffEntry>(region, false, "GraphicalStaffEntry");
             if (!entries || entries.length === 0) {
                 continue;
             } else {

commit da8f859c74aa9377d87eb74e8699d06a87dff4a0

fix getNearestStaffEntry and single line measure bounding boxes not working in minified builds (#1533, PR #1244)
both changes were recent.
getNearestStaffEntry problem was introduced recently in commit c86587eb4f454b5bbde97a5daf3aa41f64bd3a05, no issue mentioned

src/MusicalScore/Graphical/GraphicalMusicSheet.ts
@@ -689,7 +689,8 @@ export class GraphicalMusicSheet {
         for (let idx: number = 0, len: number = this.MusicPages.length; idx < len; ++idx) {
             const graphicalMusicPage: GraphicalMusicPage = this.MusicPages[idx];
             const entries: GraphicalStaffEntry[] = graphicalMusicPage.PositionAndShape.
-                getObjectsInRegion<GraphicalStaffEntry>(region, false, "GraphicalStaffEntry");
+                getObjectsInRegion<GraphicalStaffEntry>(region, false, GraphicalStaffEntry.name);
+                // note that "GraphicalStaffEntry" instead of GraphicalStaffEntry.name doesn't work with minified builds
             if (!entries || entries.length === 0) {
                 continue;
             } else {
@@ -725,7 +726,7 @@ export class GraphicalMusicSheet {
     }
 
     /** Returns nearest object of type T near clickPosition.
-     * E.g. GetNearestObject<GraphicalMeasure>(pos, "GraphicalMeasure") returns the nearest measure.
+     * E.g. GetNearestObject<GraphicalMeasure>(pos, GraphicalMeasure.name) returns the nearest measure.
      * Note that there is also GetNearestStaffEntry(), which has a bit more specific code for staff entries.
      * */
     public GetNearestObject<T extends GraphicalObject>(clickPosition: PointF2D, className: string): T {

src/MusicalScore/Graphical/GraphicalObject.ts
@@ -6,7 +6,7 @@ export class GraphicalObject extends AClassHierarchyTrackable {
     protected boundingBox: BoundingBox;
 
     public get PositionAndShape(): BoundingBox {
-        // if (this.isInstanceOfClass("GraphicalMeasure")) { // can be useful for debug
+        // if (this.isInstanceOfClass(GraphicalMeasure.name)) { // can be useful for debug
         //     console.log("get measure bbox");
         // }
         return this.boundingBox;

src/MusicalScore/Graphical/MusicSheetCalculator.ts
@@ -69,7 +69,7 @@ import { GraphicalContinuousDynamicExpression } from "./GraphicalContinuousDynam
 import { FillEmptyMeasuresWithWholeRests } from "../../OpenSheetMusicDisplay/OSMDOptions";
 import { IStafflineNoteCalculator } from "../Interfaces/IStafflineNoteCalculator";
 import { GraphicalUnknownExpression } from "./GraphicalUnknownExpression";
-import { GraphicalChordSymbolContainer } from ".";
+import { GraphicalChordSymbolContainer } from "./GraphicalChordSymbolContainer";
 import { LyricsEntry } from "../VoiceData/Lyrics/LyricsEntry";
 import { Voice } from "../VoiceData/Voice";
 import { TabNote } from "../VoiceData/TabNote";
@@ -2375,7 +2375,8 @@ export abstract class MusicSheetCalculator {
         // because the page is only available now
         if (this.rules.RenderSingleHorizontalStaffline && this.rules.RenderTitle) {
             //page.PositionAndShape.BorderRight = page.PositionAndShape.Size.width + this.rules.PageRightMargin;
-            page.PositionAndShape.calculateBoundingBox(["VexFlowMeasure"]); // ignore measures
+            page.PositionAndShape.calculateBoundingBox([GraphicalMeasure.name]); // ignore measures
+            // note: "GraphicalMeasure" instead of GraphicalMeasure.name doesn't work with minified builds (they change class names)
             // note: calculateBoundingBox by default changes measure.PositionAndShape.Size.width for some reason,
             //   inaccurate for RenderSingleHorizontalStaffline, e.g. the cursor type 3 that highlights the whole measure will get wrong width
             //   correct width was set previously via MusicSystemBuilder.setMeasureWidth().
@@ -2509,7 +2510,7 @@ export abstract class MusicSheetCalculator {
         // we need to do this again to not cut off the title for short scores:
         if (this.rules.RenderSingleHorizontalStaffline && this.rules.RenderTitle) {
             //page.PositionAndShape.BorderRight = page.PositionAndShape.Size.width + this.rules.PageRightMargin;
-            page.PositionAndShape.calculateBoundingBox(["VexFlowMeasure"]); // ignore measures
+            page.PositionAndShape.calculateBoundingBox([GraphicalMeasure.name]); // ignore measures
             // note: calculateBoundingBox by default changes measure.PositionAndShape.Size.width for some reason,
             //   inaccurate for RenderSingleHorizontalStaffline, e.g. the cursor type 3 that highlights the whole measure will get wrong width
             //   correct width was set previously via MusicSystemBuilder.setMeasureWidth().