33b54dc559fee6225782c28ff8181c28f9d525ae
sschmidTU <s.schmid@phonicscore.com>
2024-04-16 14:13:09 +0200

    fix(Cursor): Fix osmd.cursor.Iterator.CurrentRelativeInMeasureTimestamp not updated when moving/updating cursor

--- src/MusicalScore/MusicParts/MusicPartManagerIterator.ts
+++ src/MusicalScore/MusicParts/MusicPartManagerIterator.ts
@@ -533,6 +533,7 @@ export class MusicPartManagerIterator {
             const currentContainer: VerticalSourceStaffEntryContainer = this.currentMeasure.VerticalSourceStaffEntryContainers[this.currentVoiceEntryIndex];
             this.currentVoiceEntries = this.getVoiceEntries(currentContainer);
             this.currentVerticalContainerInMeasureTimestamp = currentContainer.Timestamp;
+            this.currentRelativeInMeasureTimestamp = this.currentVerticalContainerInMeasureTimestamp;
             this.currentTimeStamp = Fraction.plus(this.currentMeasure.AbsoluteTimestamp, this.currentVerticalContainerInMeasureTimestamp);
             this.activateCurrentDynamicOrTempoInstructions();
             // re-check endReached
@@ -576,6 +577,7 @@ export class MusicPartManagerIterator {
             const currentContainer: VerticalSourceStaffEntryContainer = this.currentMeasure.VerticalSourceStaffEntryContainers[this.currentVoiceEntryIndex];
             this.currentVoiceEntries = this.getVoiceEntries(currentContainer);
             this.currentVerticalContainerInMeasureTimestamp = currentContainer.Timestamp;
+            this.currentRelativeInMeasureTimestamp = this.currentVerticalContainerInMeasureTimestamp;
             this.currentTimeStamp = Fraction.plus(this.currentMeasure.AbsoluteTimestamp, this.currentVerticalContainerInMeasureTimestamp);
             const selectionEnd: Fraction = this.musicSheet.SelectionEnd;
             // TODO handle selectionEnd undefined, can happen in Beethoven Ferne Geliebte
