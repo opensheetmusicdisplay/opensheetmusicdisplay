0f5a35b1a7d8fd8fe843c4accea60a674ff4118b
sschmidTU <s.schmid@phonicscore.com>
2025-03-20 21:26:18 +0100

    chore: set version to 1.9.0-dev

--- src/OpenSheetMusicDisplay/OpenSheetMusicDisplay.ts
+++ src/OpenSheetMusicDisplay/OpenSheetMusicDisplay.ts
@@ -31,7 +31,7 @@ import { NoteEnum } from "../Common/DataObjects/Pitch";
  * After the constructor, use load() and render() to load and render a MusicXML file.
  */
 export class OpenSheetMusicDisplay {
-    protected version: string = "1.9.0-release"; // getter: this.Version
+    protected version: string = "1.9.0-dev"; // getter: this.Version
     // at release, bump version and change to -release, afterwards to -dev again
 
     /**
